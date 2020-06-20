45aa8ac561e0e7af358c837821411bb4391098df
hissatumanabinin <89464989b.c@gmail.com>
2020-06-20 09:04:43 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,6 @@
 @section('content')
     <div class='text-center'>
         <h1>倉庫管理システム</h1>
+        {!! link_to_route('signup.get','会員登録',[],['class'=>'btn btn-lg btn-primary']) !!}
     </div>
 @endsection
